7ce4d79279e1e13e1d999d84a898f1df1578232b
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-01-31 19:39:04 -0600

    commit tree 8e29a24ab8184525561ef57654b3c6b589d312f9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true  
+    validates :content, length: { minimum: 250 }  
+    validates :summary, length: { maximum: 250 }  
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }  
+
+    validate :title_is_clickbait
+
+    KEYWORDS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+    
+
+    def title_is_clickbait
+        if KEYWORDS.none? { |keyword| keyword.match title }
+            errors.add(:title, "must be clickbait")
+          end
+    end
 end
